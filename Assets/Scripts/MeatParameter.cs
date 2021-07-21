e777514c814ca43787ff2fcfa0c61076f1aa8208
YUTOKUBO2001 <kubo3900@gmail.com>
2021-07-21 10:33:29 +0900

    肉・魚類のパラメータを、保存ボタン押したときに決定する
    肉・魚類のスライダーの値を、保存ボタン押したときに変数として保存する機能を実装しました

--- Assets/Scripts/MeatParameter.cs
+++ Assets/Scripts/MeatParameter.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 public class MeatParameter : MonoBehaviour
 {
+
+    Slider meatSlider;
+
+    public static float nowMeatValue = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        meatSlider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
@@ -15,4 +23,12 @@ public class MeatParameter : MonoBehaviour
     {
         
     }
+
+    //肉・魚類のパラメータの値を決定する
+    public void Meat()
+    {
+        nowMeatValue = meatSlider.value;
+        Debug.Log("肉・魚類の値" + nowMeatValue);
+    }
+
 }
