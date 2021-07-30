58391fbee980e7279fa600025925ccbb665b0703
NinaFukase <fukase.nina@g.wakayama-u.jp>
2021-07-30 13:02:53 +0900

    ペット選択機能作成
    ペット選択を少しだけ。

--- Assets/Scripts/TitleUIManager.cs
+++ Assets/Scripts/TitleUIManager.cs
@@ -40,7 +40,7 @@ public class TitleUIManager : MonoBehaviour
     public void PushStart()
     {
         int StartPoint = PlayerPrefs.GetInt("START");
-        StartPoint++;
+        StartPoint = 0;
         Debug.Log(StartPoint);
         PlayerPrefs.SetInt("START", StartPoint);
         PlayerPrefs.Save();
